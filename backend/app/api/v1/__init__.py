041cf40481a507a0008643b6ca3a89ec2caef7cb
Niyas Hameed <niyasrad@gmail.com>
2025-04-13 13:37:13 +0530

    feat(backend): enabled MindsDB purchase prediction APIs (#11)

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -11,6 +11,7 @@ from fastapi import APIRouter, Depends
 from app.api.v1.sales import router as sales_router
 from app.api.v1.dashboard import router as dashboard_router
 from app.api.v1.catalog import router as catalog_router
+from app.api.v1.prediction import router as prediction_router
 
 from app.utils.auth.middlewares import check_api_key
 
@@ -34,5 +35,12 @@ api_router.include_router(
     catalog_router,
     prefix="/catalog",
     tags=["Catalog"],
+    dependencies=[Depends(check_api_key)],
+)
+
+api_router.include_router(
+    prediction_router,
+    prefix="/prediction",
+    tags=["Prediction"],
     dependencies=[Depends(check_api_key)],
 )
